2b943be759d6e285d44d4180b4bba1cbe2841309
champton <c.j.hampton@gmail.com>
2022-03-02 11:00:43 +0100

    fix: tidying up template and adding unsubscription

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { BookingService } from 'src/app/services/booking.service';
-import { Booking } from 'src/app/_interfaces';
-import last from 'lodash-es/last';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Location } from "@angular/common";
+import { BookingService } from "src/app/services/booking.service";
+import { Booking } from "src/app/_interfaces";
+import last from "lodash-es/last";
+import { Observable, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  bookingData: Booking | undefined;
+  bookingData!: Booking;
   panelOpenState = false;
+  bookingSubscription!: Subscription;
   constructor(private router: Router, private bookingService: BookingService) {}
 
   ngOnInit(): void {
-    this.bookingService.booking$.subscribe((response) => {
-      if (response) {
-        this.bookingData = response;
-      } else {
-        this.router.navigateByUrl('/login');
+    // TODO move to guard, or resolve block
+    this.bookingSubscription = this.bookingService.booking$.subscribe(
+      (response) => {
+        if (response) {
+          this.bookingData = response;
+        } else {
+          this.router.navigateByUrl("/login");
+        }
       }
-    });
+    );
   }
 
   get customerName() {
@@ -38,5 +43,9 @@ export class HomeComponent implements OnInit {
     return `${last(connections?.segments)?.arriveOn.city.name}, ${
       last(connections?.segments)?.arriveOn.city.country.name
     }`;
+  }
+
+  ngOnDestroy() {
+    this.bookingSubscription.unsubscribe();
   }
 }
